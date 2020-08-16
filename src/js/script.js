3748cd11e025e635d49d610286a4eb9ad660e453
Vladimir <torna7@yandex.ru>
2020-08-16 17:57:01 +0300

    add sections

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.reviews__carousel').slick({
+        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
+    });
+  });
