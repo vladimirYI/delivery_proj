1581da3696e678c3d25a049629dd9e0b78f066a2
Vladimir <torna7@yandex.ru>
2020-08-18 22:39:08 +0300

    add media header

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
     });
   });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+    })
+})
